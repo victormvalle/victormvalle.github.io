62250e321c1a61348d17955fc7a32dd1170213ef
Victor Valle <97768090+vvalle829@users.noreply.github.com>
2025-02-17 13:38:25 -0700

    script.js and style.css added

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+    darkModeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+});
